c3357d8ef3f9ec6c86fa064ccdd56b0fda74a453
earlcs <earlcasiano@gmail.com>
2020-01-21 09:39:00 +0100

    [UPD] File updated

---
+++ lib/app.rb
@@ -0,0 +1,7 @@
+require 'dotenv'# Appelle la gem Dotenv
+
+Dotenv.load('.env') # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb)
+# et grâce à la gem Dotenv, on importe toutes les données enregistrées dans un hash ENV
+
+puts ENV['SENDGRID_LOGIN']
+puts ENV['SENDGRID_PWD']
