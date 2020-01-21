4df588eca570978355fc1ae79afcffad9a83fee1
earlcs <earlcasiano@gmail.com>
2020-01-21 09:53:48 +0100

    [UPD] File updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_132011) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "formations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "is_sold", default: false, null: false
+    t.index ["user_id"], name: "index_artworks_on_user_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "formation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+    t.index ["formation_id"], name: "index_carts_on_formation_id"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "formation_id"
+    t.bigint "order_id"
+    t.integer "quantity"
+    t.string "invoice_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["formation_id"], name: "index_line_items_on_formation_id"
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["order_id"], name: "index_line_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "mobile"
+    t.string "adress"
+    t.string "city"
+    t.string "country"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +70,29 @@ ActiveRecord::Schema.define(version: 2020_01_17_132011) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_kid"
+    t.boolean "is_admin"
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.date "dateofbirth"
+    t.string "siret"
+    t.string "adress"
+    t.string "zipcode"
+    t.string "city"
+    t.string "country"
+    t.string "mobile"
+    t.string "fb_user_ID"
+    t.string "insta_user_ID"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add foreign_key "formations", "users"
+  add foreign_key "carts", "users"
+  add foreignkey "line_items", "formation"
+  add foreign_key "line_items", "carts"
+  add foreign_key "line_items", "orders"
+  add foreign_key "orders", "users"
 
 end
